addc9b655d23997f7fc36b8a7fb264c999f3deb7
madhavkotcherla <madhavkotcherla@gmail.com>
2026-03-09 21:48:22 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
     if (err) {
